96756000682e716977e018aa2f243a5f56cb835e
oRl952 <meghladighi14@gmail.com>
2025-07-30 23:54:06 +0600

    Add files via upload

---
+++ source/src/test/java/com/example/bal/BalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
