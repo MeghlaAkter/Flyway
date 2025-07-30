0b5f065271c716caa0b4a1379e11e7ede263c65e
oRl952 <meghladighi14@gmail.com>
2025-07-30 23:57:47 +0600

    Add files via upload

---
+++ src/main/java/com/example/bal/BalApplication.java
@@ -0,0 +1,13 @@
+package com.example.bal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalApplication.class, args);
+	}
+
+}
